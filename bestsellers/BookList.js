d5b176a0a031c4e5b4ba00a7f81312edaf0aedf3
Jong <yomybaby@yomybaby.com>
2018-11-25 18:17:34 +0800

    Navigation기본 세팅 및 bookItem에 star 버튼 추가

--- bestsellers/BookList.js
+++ bestsellers/BookList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 import {
   StyleSheet,
@@ -6,12 +6,12 @@ import {
   View,
   Image,
   FlatList,
-  ActivityIndicator
-} from "react-native";
+  ActivityIndicator,
+} from 'react-native';
 
-import BookItem from "./BookItem";
-import NYT from "./NYT";
-import NAVER from "./NAVER";
+import BookItem from './BookItem';
+import NYT from './NYT';
+import NAVER from './NAVER';
 
 class BookList extends Component {
   constructor(props) {
@@ -20,7 +20,7 @@ class BookList extends Component {
       data: [],
       refreshing: false,
       loadingMore: false,
-      lastPage: 1
+      lastPage: 1,
     };
     this.onEndReachedCalledDuringMomentum = true;
   }
@@ -31,7 +31,15 @@ class BookList extends Component {
 
   _renderItem = ({ item }) => {
     return (
-      <BookItem coverURL={item.image} title={item.title} author={item.author} />
+      <BookItem
+        coverURL={item.image}
+        title={item.title}
+        author={item.author}
+        isFavorite={item.isFavorite}
+        onPressFavorite={() => {
+          console.log(item.key);
+        }}
+      />
     );
   };
 
@@ -46,14 +54,14 @@ class BookList extends Component {
 
   _refreshData = () => {
     this.setState({
-      refreshing: true
+      refreshing: true,
     });
 
     NAVER.fetchBooks().then(books => {
       this.setState({
         data: this._addKeysToBooks(books),
         refreshing: false,
-        lastPage: 1
+        lastPage: 1,
       });
     });
   };
@@ -62,7 +70,7 @@ class BookList extends Component {
     if (!this.onEndReachedCalledDuringMomentum) {
       console.log(distanceFromEnd);
       this.setState({
-        loadingMore: true
+        loadingMore: true,
       });
 
       NAVER.fetchBooks(this.state.lastPage + 1).then(books => {
@@ -70,7 +78,7 @@ class BookList extends Component {
         this.setState(state => ({
           data: state.data.concat(this._addKeysToBooks(books)),
           lastPage: state.lastPage + 1,
-          loadingMore: false
+          loadingMore: false,
         }));
       });
       this.onEndReachedCalledDuringMomentum = true;
@@ -83,8 +91,8 @@ class BookList extends Component {
         style={{
           height: 50,
           flex: 1,
-          alignItems: "center",
-          justifyContent: "center"
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
         <ActivityIndicator animating={this.state.loadingMore} size="small" />
