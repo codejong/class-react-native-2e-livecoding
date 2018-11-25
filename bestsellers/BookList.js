0589ceefafe542a28ca9055161358671d5ec6b9f
Jong <yomybaby@yomybaby.com>
2018-11-25 20:18:31 +0800

    책 검색 텍스트 인풋 추가

--- bestsellers/BookList.js
+++ bestsellers/BookList.js
@@ -7,6 +7,7 @@ import {
   Image,
   FlatList,
   ActivityIndicator,
+  TextInput,
 } from 'react-native';
 import { connect } from 'react-redux';
 import BookItem from './BookItem';
@@ -21,6 +22,7 @@ class BookList extends Component {
       refreshing: false,
       loadingMore: false,
       lastPage: 1,
+      keyword: 'react',
     };
     this.onEndReachedCalledDuringMomentum = true;
   }
@@ -61,7 +63,10 @@ class BookList extends Component {
       refreshing: true,
     });
 
-    NAVER.fetchBooks().then(books => {
+    NAVER.fetchBooks({
+      page: 1,
+      keyword: this.state.keyword,
+    }).then(books => {
       this.setState({
         data: this._addKeysToBooks(books),
         refreshing: false,
@@ -77,7 +82,10 @@ class BookList extends Component {
         loadingMore: true,
       });
 
-      NAVER.fetchBooks(this.state.lastPage + 1).then(books => {
+      NAVER.fetchBooks({
+        page: this.state.lastPage + 1,
+        keyword: this.state.keyword,
+      }).then(books => {
         console.log(books);
         this.setState(state => ({
           data: state.data.concat(this._addKeysToBooks(books)),
@@ -111,6 +119,27 @@ class BookList extends Component {
     }));
     return (
       <View style={styles.container}>
+        <View style={{ height: 50 }}>
+          <TextInput
+            style={{
+              borderColor: 'gray',
+              borderWidth: 1,
+              height: 30,
+              margin: 10,
+            }}
+            onSubmitEditing={e => {
+              this.setState(
+                {
+                  keyword: e.nativeEvent.text,
+                },
+                () => {
+                  this._refreshData();
+                }
+              );
+            }}
+            defaultValue={this.state.keyword}
+          />
+        </View>
         <FlatList
           data={listDataWithFavorite}
           renderItem={this._renderItem}
--- bestsellers/NAVER.js
+++ bestsellers/NAVER.js
@@ -1,16 +1,19 @@
-const API_KEY = "73b19491b83909c7e07016f4bb4644f9:2:60667290";
-const LIST_NAME = "hardcover-fiction";
-const API_STEM = "https://api.nytimes.com/svc/books/v3/lists";
+const API_KEY = '73b19491b83909c7e07016f4bb4644f9:2:60667290';
+const LIST_NAME = 'hardcover-fiction';
+const API_STEM = 'https://api.nytimes.com/svc/books/v3/lists';
 
-function fetchBooks(page = 1) {
+function fetchBooks({ page = 1, keyword = 'node' }) {
   const display = 10;
-  const start = display * (page-1) + 1;
-  return fetch(`https://openapi.naver.com/v1/search/book.json?query=javascript&display=${display}&start=${start}`, {
-    headers : {
-      'X-Naver-Client-Id': 'vTiJhVKTgkFtmAOe1aRw',
-      'X-Naver-Client-Secret': 'KNnOp1CgQd'
+  const start = display * (page - 1) + 1;
+  return fetch(
+    `https://openapi.naver.com/v1/search/book.json?query=${keyword}&display=${display}&start=${start}`,
+    {
+      headers: {
+        'X-Naver-Client-Id': 'vTiJhVKTgkFtmAOe1aRw',
+        'X-Naver-Client-Secret': 'KNnOp1CgQd',
+      },
     }
-  })
+  )
     .then(response => response.json())
     .then(responseJson => {
       return responseJson.items;
@@ -20,5 +23,5 @@ function fetchBooks(page = 1) {
     });
 }
 
-fetchBooks(1).then(console.dir)
+fetchBooks(1).then(console.dir);
 export default { fetchBooks: fetchBooks };
